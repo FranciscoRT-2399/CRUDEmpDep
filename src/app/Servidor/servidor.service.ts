193f12d6ec4f1669de625381466afcae07f9fcb7
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-19 08:13:44 -0600

    Settings del proyecto

--- src/app/Servidor/servidor.service.ts
+++ src/app/Servidor/servidor.service.ts
@@ -1,9 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Empleados } from '../Entidades/Empleado';
+import { Departamentos } from '../Entidades/Departamento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServidorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+	url = 'http://localhost:8003/api'
+
+	listarEmp(){
+		return this.http.get<Empleados[]>(this.url + "/Empleado/listar");
+	}
+
+	buscarEmp(empleado: Empleados){
+		return this.http.post<Empleados>(this.url + "/Empleado/buscar", empleado);
+	}
+
+	guardarEmp(empleado: Empleados){
+		return this.http.post<String>(this.url + "/Empleado/guardar", empleado);
+	}
+
+	editarEmp(empleado: Empleados){
+		return this.http.post<String>(this.url + "/Empleado/editar", empleado);
+	}
+
+	eliminarEmp(empleado: Empleados){
+		return this.http.post<String>(this.url + "/Empleado/eliminar", empleado);
+	}
+
+	buscarPorPuesto(puesto: String){
+		return this.http.post<Empleados[]>(`${this.url}/Empleado/buscarPorPuesto?puesto=${puesto}`,{});
+	}
+
+	// * DEPARTAMENTOS ---------------------------------------------------------------
+
+	listarDep(){
+		return this.http.get<Departamentos[]>(this.url + "/Departamento/listar");
+	}
+
+	buscarDep(departamento: Departamentos){
+		return this.http.post<Departamentos>(this.url + "/Departamento/buscar", departamento);
+	}
+	
+	guardarDep(departamento: Departamentos){
+		return this.http.post<String>(this.url + "/Departamento/guardar", departamento);
+	}
+
+	editarDep(departamento: Departamentos){
+		return this.http.post<String>(this.url + "/Departamento/editar", departamento);
+	}
+	
+	eliminarDep(departamento: Departamentos){
+		return this.http.post<String>(this.url + "/Departamento/eliminar", departamento);
+	}
+	
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { ListarComponent } from './Componentes/Empleados/listar/listar.component';
+import { ListarDepComponent } from './Componentes/Departamentos/listar-dep/listar-dep.component';
+import { GuardarComponent } from './Componentes/Empleados/guardar/guardar.component';
+import { GuardarDepComponent } from './Componentes/Departamentos/guardar-dep/guardar-dep.component';
+import { EditarComponent } from './Componentes/Empleados/editar/editar.component';
+import { EditarDepComponent } from './Componentes/Departamentos/editar-dep/editar-dep.component';
+import { EliminarComponent } from './Componentes/Empleados/eliminar/eliminar.component';
+import { EliminarDepComponent } from './Componentes/Departamentos/eliminar-dep/eliminar-dep.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'Empleado/listar',
+		component: ListarComponent
+	},
+	{
+		path: 'Departamento/listar',
+		component: ListarDepComponent
+	},
+	{
+		path: 'Empleado/guardar',
+		component: GuardarComponent
+	},
+	{
+		path: 'Departamento/guardar',
+		component: GuardarDepComponent
+	},
+	{
+		path: 'Empleado/editar',
+		component: EditarComponent
+	},
+	{
+		path: 'Departamento/editar',
+		component: EditarDepComponent
+	},
+	{
+		path: 'Empleado/eliminar',
+		component: EliminarComponent
+	},
+	{
+		path: 'Departamento/eliminar',
+		component: EliminarDepComponent
+	}
+];
