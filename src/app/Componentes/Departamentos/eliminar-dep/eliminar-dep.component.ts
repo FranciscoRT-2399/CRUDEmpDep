e512c8ba8690be000015f726b1544f43928e3ef3
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-19 08:30:50 -0600

    update eliminar

--- src/app/Componentes/Departamentos/eliminar-dep/eliminar-dep.component.ts
+++ src/app/Componentes/Departamentos/eliminar-dep/eliminar-dep.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServidorService } from '../../../Servidor/servidor.service';
+import { Departamentos } from '../../../Entidades/Departamento';
 
 @Component({
   selector: 'app-eliminar-dep',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './eliminar-dep.component.html',
   styleUrl: './eliminar-dep.component.css'
 })
-export class EliminarDepComponent {
+export class EliminarDepComponent implements OnInit{
+
+	constructor(private router: Router, private service: ServidorService){}
+
+	depa : Departamentos = new Departamentos();
+
+	ngOnInit(): void {
+		this.buscar();
+	}
+
+	buscar(){
+		let id = localStorage.getItem('idDep');
+		this.depa.idDep = Number(id);
+		this.service.buscarDep(this.depa).subscribe(data =>{
+			this.depa = data;
+		})
+	}
+
+	eliminar(){
+		this.service.eliminarDep(this.depa).subscribe(data =>{
+			this.router.navigate(['Departamento/listar']);
+		})
+	}
+
+	cancelar(){
+		this.router.navigate(['Departamento/listar']);
+	}
 
 }
